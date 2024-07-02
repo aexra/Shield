85d1cef23da33ad90bca2f4206bc518db7c99927
Никита Фомин <defdXs@yandex.ru>
2024-07-02 23:51:43 +0300

    Fixed everything weird. Now hosts on http 5277 port

---
+++ Shield.App/Helpers/ApiHelper.cs
@@ -0,0 +1,39 @@
+﻿using System.Net.Http.Json;
+using Microsoft.Extensions.Primitives;
+using Windows.Storage;
+
+namespace Shield.App.Helpers;
+public class ApiHelper
+{
+    private static string baseAddress = "http://127.0.0.1:5277/api";
+    private static HttpClient sharedClient = new()
+    {
+        
+        
+    };
+    private static string? token => (string?)ApplicationData.Current.LocalSettings.Values["apiToken"];
+
+    public static async Task<string?> TryLogin(string name, string password)
+    {
+        using HttpRequestMessage request = new HttpRequestMessage();
+        request.RequestUri = new Uri($"{baseAddress}/user/login");
+        request.Method = HttpMethod.Post;
+        request.Content = JsonContent.Create(new LoginDto() { UserName=name, Password=password });
+
+        try
+        {
+            var response = await sharedClient.SendAsync(request);
+            return response.Content.ToString();
+        }
+        catch (Exception ex)
+        {
+            return ex.Message + (ex.InnerException != null ? "\n" + ex.InnerException.Message : "");
+        }
+    }
+
+    private class LoginDto
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Shield.App/Views/ContractsPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using Microsoft.Windows.ApplicationModel.Resources;
+using Shield.App.Helpers;
+using Shield.App.ViewModels;
+
+namespace Shield.App.Views;
+
+public sealed partial class ContractsPage : Page
+{
+    public ContractsViewModel ViewModel
+    {
+        get;
+    }
+
+    public ContractsPage()
+    {
+        ViewModel = App.GetService<ContractsViewModel>();
+        InitializeComponent();
+    }
+
+    private async void Button_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+    {
+        var response = await ApiHelper.TryLogin("a", "b");
+        TB.Text = response ?? "no response";
+    }
+}
