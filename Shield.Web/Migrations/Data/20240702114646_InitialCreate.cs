0c561bda8ee3e7e7c605bcacdf1a2061d75222c4
Никита Фомин <defdXs@yandex.ru>
2024-07-02 14:48:03 +0300

    Created initial databases for identity and data

---
+++ Shield.Web/Migrations/Data/20240702114646_InitialCreate.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Shield.Web.Migrations.Data
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contracts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Address = table.Column<string>(type: "TEXT", nullable: false),
+                    PlanSrc = table.Column<string>(type: "TEXT", nullable: false),
+                    Owners = table.Column<string>(type: "TEXT", nullable: false),
+                    Bailee = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contracts", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contracts");
+        }
+    }
+}
