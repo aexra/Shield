a65b76d59bbbd088e6a62b93145b6a3ca344116d
Никита Фомин <defdXs@yandex.ru>
2024-07-06 14:26:39 +0300

    Added some UI elements

--- Shield.App/Views/ContractsPage.xaml.cs
+++ Shield.App/Views/ContractsPage.xaml.cs
@@ -19,29 +19,4 @@ public sealed partial class ContractsPage : Page
         ViewModel = App.GetService<ContractsViewModel>();
         InitializeComponent();
     }
-
-    private async void Button_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
-    {
-        //var response = await ApiHelper.TryLogin("awufka", "Lutya1_");
-
-        //if (response != null)
-        //{
-        //    if (response is Exception ex)
-        //    {
-        //        TB.Text = ex.Message;
-        //        if (ex.InnerException != null)
-        //        {
-        //            TB.Text += "\n" + ex.InnerException.Message;
-        //        }
-        //    }
-        //    else if (response is LoginResponseDto dto)
-        //    {
-
-        //    }
-        //}
-
-        //TB.Text = (string?)ApplicationData.Current.LocalSettings.Values["apiToken"];
-
-        TB.Text = string.Join("\n", (await ApiHelper.GetAllContracts()).Contracts.Select(c => $"ID={c.Id}\nAddress={c.Address}\nOwners={c.Owners}\nBailee={c.Bailee}"));
-    }
 }
