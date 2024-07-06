f3c85e1a9b67e99078a5281145d4e052a0ceb01b
Никита Фомин <defdXs@yandex.ru>
2024-07-06 16:59:26 +0300

    Added some buttons functionality

--- Shield.App/Views/ContractsPage.xaml.cs
+++ Shield.App/Views/ContractsPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.UI.Xaml.Controls;
+﻿using System.Collections.ObjectModel;
+using Microsoft.UI.Xaml.Controls;
 using Microsoft.Windows.ApplicationModel.Resources;
 using Shield.App.Controls;
 using Shield.App.Helpers;
@@ -10,6 +11,8 @@ namespace Shield.App.Views;
 
 public sealed partial class ContractsPage : Page
 {
+    private ObservableCollection<ContractControl> contractControls = new();
+
     public ContractsViewModel ViewModel
     {
         get;
@@ -21,10 +24,36 @@ public sealed partial class ContractsPage : Page
         InitializeComponent();
     }
 
-    private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    private async void CreateContractBtn_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
     {
-        var lv = (ListView)sender;
-        var si = lv.SelectedItem;
-        
+
+    }
+
+    private async void EditContractBtn_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+    {
+
+    }
+
+    private async void MoreContractBtn_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+    {
+
+    }
+
+    private async void UpdateContractsListBtn_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+    {
+        var response = await ApiHelper.GetAllContracts();
+
+        if (response != null)
+        {
+            foreach (var contract in response.Contracts)
+            {
+                contractControls.Add(new(contract));
+            }
+        }
+    }
+
+    private async void DeleteContractBtn_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+    {
+
     }
 }
