a6da0c20215e8852596743d65b9956e9fc356dc5
Никита Фомин <defdXs@yandex.ru>
2024-07-06 18:01:02 +0300

    Removed useless old folder

--- DataAccess/Models/LoginDto.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace DataAccess.Models;
-public class LoginDto
-{
-    public string UserName { get; set; }
-    public string Email { get; set; }
-    public string Token { get; set; }
-}
--- DataAccess/Models/RegisterDto.cs
+++
@@ -1,4 +0,0 @@
-﻿namespace DataAccess.Models;
-public class RegisterDto
-{
-}
