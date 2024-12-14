891ab07d95429ba0df135a8366ace277cbf8325a
Nikita Fomin <defdXs@yandex.ru>
2024-12-14 13:30:12 +0300

    aaaaaaaaaa

---
+++ Shield.Web/Data/Contexts/VanguardContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Vanguard.Web.Data.Models;
+
+namespace Vanguard.Web.Data.Contexts;
+
+public class IdentityContext : IdentityDbContext<User>
+{
+    public IdentityContext()
+    {
+        
+    }
+
+    protected override async void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql($"Host=localhost;Port=5432;Database=postgres;Username=postgres;Password=1234;Include Error Detail=True");
+    }
+}
